39021d1724e9d696040ba0ae5e3e220bfa70e8b0
Egor <egormaxb@gmail.com>
2024-05-14 20:37:09 -0700

    Added Collections for projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), mdx()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel()
 });
