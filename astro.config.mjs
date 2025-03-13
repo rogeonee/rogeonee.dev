504418290f1aac2a5d800f7f4198b2d49796b992
Egor <egormaxb@gmail.com>
2025-03-13 10:57:53 -0700

    chore: more config updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: "server",
-  adapter: vercel()
+  site: 'https://www.rogeonee.com',
+  output: 'static',
+  adapter: vercel(),
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'load'
+  },
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false
+    })
+  ],
 });
